46195512adc83ba28b3f91ff9407f106850d2e65
sudiddo <su.diddo@gmail.com>
2021-03-08 16:12:14 +0700

    add mobile first

--- src/App.js
+++ src/App.js
@@ -107,14 +107,17 @@ function App() {
   };
 
   return (
-    <div className="bg-gray-600 min-h-screen text-white">
+    <div className="bg-gray-600 text-white">
       <header>
         <h1 className="p-12 text-white text-center font-bold text-4xl">{`SRW&Co Technical Test`}</h1>
       </header>
-      <main>
-        <div className="grid grid-cols-3 gap-3 bg-gray-900 mx-5">
-          <div className="bg-red-900 flex flex-col items-center justify-start pt-6">
-            <form onSubmit={checkPalindrome}>
+      <main className="justify-center">
+        <div className="grid lg:grid-cols-3 grid-rows-2 gap-3 mx-2 lg:pb-0 pb-28">
+          <div>
+            <form
+              className="bg-red-900 py-6 lg:px-5 px-3 h-full w-full rounded-md"
+              onSubmit={checkPalindrome}
+            >
               <h3 className="text-xl">1. Palindrome Test</h3>
               <div>
                 Enter words:
@@ -138,89 +141,96 @@ function App() {
               </div>
             ) : null}
           </div>
-          <div className="flex flex-col px-10 justify-start pt-6">
-            <form onSubmit={sortAscending}>
-              <h3 className="text-xl">2. Sort Ascending</h3>
-              <div className="text-center mt-5">{`[${numArray}] = [${sortedNumArray}]`}</div>
-              <input
-                className="bg-gray-200 float-right rounded-md text-black mt-5 px-5 py-1"
-                type="submit"
-                value="Sort"
-              />
-            </form>
-            <form
-              className="border border-white p-3 mt-5 rounded-md"
-              onSubmit={sortDIYAscending}
-            >
-              <h5> Input your own number! (please seperate with comma)</h5>
-              <div>
+          <div className="flex flex-col justify-start ">
+            <div className="bg-gray-900 h-full lg:px-10 px-5 py-6 rounded-md">
+              <form onSubmit={sortAscending}>
+                <h3 className="text-xl">2. Sort Ascending</h3>
+                <div className="text-center mt-5">{`[${numArray}] = [${sortedNumArray}]`}</div>
+                <input
+                  className="bg-gray-200  float-right rounded-md text-black mt-5 px-5 py-1"
+                  type="submit"
+                  value="Sort"
+                />
+              </form>
+
+              <form
+                className="border border-white p-3 mt-20 rounded-md"
+                onSubmit={sortDIYAscending}
+              >
+                <h5> Input your own number! (please seperate with comma)</h5>
                 <div>
+                  <div>
+                    <input
+                      placeholder="Example 7,3,4,5"
+                      className="bg-gray-200 rounded-md py-1 text-black mt-5 px-1 lg:w-56 w-36"
+                      type="text"
+                      value={diySortedNumArray}
+                      onChange={(e) => setDiySortedNumArray(e.target.value)}
+                    />
+                    {` = [${diySortedNumArrayResult}]`}
+                  </div>
+                </div>
+                <div className="flex justify-end">
+                  <input
+                    className="bg-gray-200 mt-5 rounded-md text-black px-5 py-1"
+                    type="submit"
+                    value="Sort"
+                  />
+                </div>
+                <div style={{ color: "red", marginTop: 5 }}>
+                  {diySortedErrorMessage}
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className=" flex flex-col justify-start ">
+            <div className="bg-red-900 py-6 px-3 rounded-md">
+              <h3 className="text-xl">3. Symmetric Difference</h3>
+              <form
+                style={{ marginTop: 10, marginBottom: 20 }}
+                onSubmit={SymDiffCount}
+              >
+                <div>{`Array 1: ${symDiffA}`}</div>
+                <div>{`Array 2: ${symDiffB}`}</div>
+                <div>{`Symmetric Difference: ${symDiffResult}`}</div>
+                <input
+                  className="bg-gray-200 float-right rounded-md text-black mt-5 px-5 py-1"
+                  type="submit"
+                  value="Calculate"
+                />
+              </form>
+              <form className="mt-20" onSubmit={DiySymDiffCount}>
+                <h5> Input your own number! (please seperate with comma)</h5>
+                <div>
+                  {`Array 1: `}
                   <input
                     placeholder="Example 7,3,4,5"
-                    className="bg-gray-200 rounded-md py-1 text-black mt-5 px-1"
+                    className="bg-gray-200 ml-3 rounded-md py-1 text-black mt-5 px-1"
                     type="text"
-                    value={diySortedNumArray}
-                    onChange={(e) => setDiySortedNumArray(e.target.value)}
+                    value={diyDiffSymA}
+                    onChange={(e) => setDiyDiffSymA(e.target.value)}
                   />
-                  {` = [${diySortedNumArrayResult}]`}
                 </div>
-              </div>
-              <input
-                className="bg-gray-200 float-right rounded-md text-black mt-5 px-5 py-1"
-                type="submit"
-                value="Sort"
-              />
-              <div style={{ color: "red", marginTop: 5 }}>
-                {diySortedErrorMessage}
-              </div>
-            </form>
-          </div>
-          <div className="bg-red-900 flex flex-col justify-start py-6 px-10">
-            <h3 className="text-xl">3. Symmetric Difference</h3>
-            <form
-              style={{ marginTop: 10, marginBottom: 20 }}
-              onSubmit={SymDiffCount}
-            >
-              <div>{`Array 1: ${symDiffA}`}</div>
-              <div>{`Array 2: ${symDiffB}`}</div>
-              <div>{`Symmetric Difference: ${symDiffResult}`}</div>
-              <input
-                className="bg-gray-200 float-right rounded-md text-black mt-5 px-5 py-1"
-                type="submit"
-                value="Calculate"
-              />
-            </form>
-            <h5> Input your own number! (please seperate with comma)</h5>
-            <form style={{ marginTop: 10 }} onSubmit={DiySymDiffCount}>
-              <div>
-                {`Array 1: `}
+                <div>
+                  {`Array 2: `}
+                  <input
+                    placeholder="Example 7,3,4,5"
+                    className="bg-gray-200 ml-3 rounded-md py-1 text-black mt-5 px-1"
+                    type="text"
+                    value={diyDiffSymB}
+                    onChange={(e) => setDiyDiffSymB(e.target.value)}
+                  />
+                </div>
+                <div
+                  style={{ marginTop: 10 }}
+                >{`Symmetric Difference: ${diySymDiffResult}`}</div>
                 <input
-                  placeholder="Example 7,3,4,5"
-                  className="bg-gray-200 ml-3 rounded-md py-1 text-black mt-5 px-1"
-                  type="text"
-                  value={diyDiffSymA}
-                  onChange={(e) => setDiyDiffSymA(e.target.value)}
+                  className="bg-gray-200 float-right rounded-md text-black mt-5 px-5 py-1"
+                  type="submit"
+                  value="Calculate"
                 />
-              </div>
-              <div>
-                {`Array 2: `}
-                <input
-                  placeholder="Example 7,3,4,5"
-                  className="bg-gray-200 ml-3 rounded-md py-1 text-black mt-5 px-1"
-                  type="text"
-                  value={diyDiffSymB}
-                  onChange={(e) => setDiyDiffSymB(e.target.value)}
-                />
-              </div>
-              <div
-                style={{ marginTop: 10 }}
-              >{`Symmetric Difference: ${diySymDiffResult}`}</div>
-              <input
-                className="bg-gray-200 float-right rounded-md text-black mt-5 px-5 py-1"
-                type="submit"
-                value="Calculate"
-              />
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </main>
